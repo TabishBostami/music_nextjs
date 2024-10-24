d2847ac95f63d55b5317a7b03896a5671c5b7588
Tabish bostami <tabish.bostami042000@gamil.com>
2024-10-24 14:19:44 +0530

    pageNlayout

--- src/app/chai/page.tsx
+++ src/app/chai/page.tsx
@@ -1,5 +1,9 @@
 export default function Chaipage(){
   return(
-    <h1 className="text-3xl text-center mt-8">Chai page</h1>
+    <>
+       <h1 className="text-3xl text-center mt-8">Chai page</h1>
+       <h2>How are you khana kha ke jaana</h2>
+    </>
+   
   )
 }
